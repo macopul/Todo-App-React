c8f3ca925f3723cddcc20a872e436ccc10fcc14d
Maciej Opulski <maciopul@gmail.com>
2023-11-22 12:17:28 +0100

    removed unnecesary code

--- src/themeMode/ThemeMode.tsx
+++ src/themeMode/ThemeMode.tsx
@@ -1,10 +1,7 @@
 import { THEME_MODE_KEY } from '../constants/storage';
 
 export const getIsDarkMode = () => {
-  // return JSON.parse(window.localStorage.getItem(THEME_MODE_KEY) || '');
-  // return JSON.parse(window.localStorage.getItem(THEME_MODE_KEY) || '');
   return JSON.parse(window.localStorage.getItem(THEME_MODE_KEY) || 'false');
-  // return window.localStorage.getItem(THEME_MODE_KEY);
 };
 
 export const setDarkMode = () => {
